bb3f4880bc0628631cf7e9da5123b377d5356702
Tushar Gajwani <89583890+TusharG7@users.noreply.github.com>
2024-08-12 19:21:42 +0530

    Deploy

--- server/app.js
+++ server/app.js
@@ -5,6 +5,11 @@ import { errorMiddleware } from "./middlewares/error.js";
 import reservationRouter from "./routes/reservationRoute.js";
 import appointmentRouter from "./routes/appointmentRoute.js";
 import { dbConnection } from "./database/dbConnection.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
 
 const app = express();
 dotenv.config({ path: "./config.env" });
@@ -18,6 +23,10 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(_dirname, "/client/dist")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(_dirname, "/client/dist/index.html"))
+);
 
 app.use("/api/reservation", reservationRouter);
 app.use("/api/appointment", appointmentRouter);
